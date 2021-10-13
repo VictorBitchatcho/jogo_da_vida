8b099cbefc09433c3d7f9914e21e05e5111b4a87
RVCoelho <80905108+RVCoelho@users.noreply.github.com>
2021-10-13 11:45:10 -0300

    Add files via upload

---
+++ jdv_main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "jdv_model.c"
+#include "jdv_view.c"
+#include "jdv_control.c"
+
+int main()
+{
+    jogoDaVida();
+    return 0;
+}

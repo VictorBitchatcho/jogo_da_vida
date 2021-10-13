90cb59c94b049244fdd4c7cc070b9f175cad911a
RVCoelho <80905108+RVCoelho@users.noreply.github.com>
2021-10-13 12:35:57 -0300

    Add files via upload

---
+++ jdv_view.c
@@ -0,0 +1,17 @@
+int menu();
+
+
+int menu()
+{
+    int x;
+    printf("    JOGO DA VIDA\n");
+    printf("---------------------------------\n");
+    printf("1 - Criar um novo mundo\n");
+    printf("2 - Preparar e salvar um mundo inicial\n");
+    printf("3 - Recuperar um mundo inicial anterior\n");
+    printf("4 - Iniciar o mundo\n");
+    printf("5 - Mostrar o mundo final apos o processo\n");
+    printf("6 - Limpar o mundo\n");
+    printf("0 - Sair\n");
+    return x;
+}

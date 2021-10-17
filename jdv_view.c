9546c04eb00b76a99d105fcd856eb18f4f2a18a8
VictorBitchatcho <92453678+VictorBitchatcho@users.noreply.github.com>
2021-10-17 19:09:46 -0300

    Update jdv_view.c

--- jdv_view.c
+++ jdv_view.c
@@ -1,17 +1,12 @@
-int menu();
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include "JVIDA-JJVR-Controller.c"
+#include "JVIDA-JJVR-Model.c"
+#include "JVIDA-JJVR-View.c"
 
-
-int menu()
+int main()
 {
-    int x;
-    printf("    JOGO DA VIDA\n");
-    printf("---------------------------------\n");
-    printf("1 - Criar um novo mundo\n");
-    printf("2 - Preparar e salvar um mundo inicial\n");
-    printf("3 - Recuperar um mundo inicial anterior\n");
-    printf("4 - Iniciar o mundo\n");
-    printf("5 - Mostrar o mundo final apos o processo\n");
-    printf("6 - Limpar o mundo\n");
-    printf("0 - Sair\n");
-    return x;
+    jogoDaVida();
+    return 0;
 }

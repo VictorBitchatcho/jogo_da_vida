bb13e625cccd4270e8fcf85449b5418cdd2f2067
VictorBitchatcho <92453678+VictorBitchatcho@users.noreply.github.com>
2021-10-17 19:11:09 -0300

    Update jdv_control.c

--- jdv_control.c
+++ jdv_control.c
@@ -1,3 +1,7 @@
+#include <stdio.h>
+#include "JVIDA-JJVR-Model.c"
+#include "JVIDA-JJVR-View.c"
+
 void jogoDaVida();
 
 
@@ -12,25 +16,25 @@ void jogoDaVida()
             case 1:
                 geraMundo();
                 break;
-            
+
             case 2:
                 break;
-            
+
             case 3:
                 break;
-            
+
             case 4:
                 break;
-            
+
             case 5:
                 break;
-            
+
             case 6:
                 break;
-            
+
             case 0:
                 break;
-            
+
         }
     }
 }
@@ -38,17 +42,22 @@ void jogoDaVida()
 void geraMundo()
 {
     int i, k, tam, j=0;
-    
+
     printf("Escolha o tamanho do mundo (maximo: 100): ");
     scanf("%d", &tam);
-    
+
     for(i=0;i<=tam;i++)
-        printf("%2.d", j);
+        printf("%2.d", i);
 
     for(i=0;i<=tam;i++)
     {
-        for(k=;k<=tam;k++)
+        printf("%2.d", k);
+
+        for(k=0;k<=tam;k++)
         {
-            
+            printf(".\t");
+
         }
+        printf("\n");
+    }
 }
